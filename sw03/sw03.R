e79ed8c4492776fc92a7387df81d2ccd8efcad62
TheCell <simon_hischier@hotmail.com>
2017-10-03 14:43:19 +0200

    Prepare SW03

---
+++ sw03/sw03.R
@@ -0,0 +1,9 @@
+#setwd("C:/Users/simon/Google Drive/HSLU/STOC/StochasticWithR/sw03/")
+#data <- read.table(file="Schokolade_Nobelpreis.txt", sep=" ", header=TRUE)
+
+# SW 3
+# Aufgabe 1
+
+Messungen <- c(rep(2, 18), rep(5, 17))
+summary(Messungen)
+sum(Messungen)
