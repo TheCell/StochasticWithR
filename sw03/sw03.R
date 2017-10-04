57de73196cb023fb7d3dfb4e9c6d58c6d98cea38
TheCell <simon_hischier@hotmail.com>
2017-10-04 15:33:16 +0200

    SW3 work in school

--- sw03/sw03.R
+++ sw03/sw03.R
@@ -2,8 +2,42 @@
 #data <- read.table(file="Schokolade_Nobelpreis.txt", sep=" ", header=TRUE)
 
 # SW 3
-# Aufgabe 1
 
-Messungen <- c(rep(2, 18), rep(5, 17))
-summary(Messungen)
-sum(Messungen)
+# Aufgabe 1
+# a
+#Messungen <- c(rep(2, 18), rep(5, 17))
+anzahl.zerfaelle <- seq(2,17,by=1)
+anzahl.beobachtungen <- c(18,28,56,105,126,146,164,161,123,101,74,53,23,15,9,5)
+messungen <- rep(anzahl.zerfaelle,anzahl.beobachtungen)
+sum(messungen)
+
+# b
+hist(messungen,breaks=c(0,anzahl.zerfaelle[-16],25),
+     xlab="Anzahl Zerfaelle in 10 Sekunden", main="messungen", freq=TRUE)
+box()
+
+# Balkenfläche ist die Relative Häufigkeit
+hist(messungen,breaks=c(0,anzahl.zerfaelle[-16],25),
+     xlab="Anzahl Zerfaelle in 10 Sekunden", main="messungen", freq=FALSE)
+box()
+
+# c
+wahrscheinlichkeiten <- anzahl.beobachtungen/sum(anzahl.beobachtungen)
+plot(anzahl.zerfaelle,wahrscheinlichkeiten,type="h",
+     xlab="Anzahl Zerfaelle in 10 Sekunden", ylab="Wahrscheinlichkeit")
+# e
+mean(messungen)
+median(messungen)
+sd(messungen)
+
+plot(anzahl.zerfaelle,wahrscheinlichkeiten,type="h",
+     xlab="Anzahl Zerfaelle in 10 Sekunden", ylab="Wahrscheinlichkeit")
+abline(v=8.37,col="red")
+lines(x=c(8.37-2.92,8.37+2.92),y=c(0.08,0.08),col="blue")
+
+# f
+# kumulative Verteilungsfunktion
+n <- length(messungen)
+plot(sort(messungen),(1:n)/n, type="s",ylim=c(0,1),
+     xlab="Anzahl Zerfaelle in 10 Sekunden")
+abline(h=0.5)
