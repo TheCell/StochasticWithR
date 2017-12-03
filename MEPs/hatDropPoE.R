3223004369d2c49b9e9ce0a31632b9943918b5aa
TheCell <simon_hischier@hotmail.com>
2017-12-04 00:19:29 +0100

    made some PoE calculations

---
+++ MEPs/hatDropPoE.R
@@ -0,0 +1,10 @@
+1 - dpois(0, 0.96)
+ppois(1, 0.96)
+ppois(0, 0.96)
+1 - ppois(0, 0.96)
+1 - ppois(0, 1.92)
+1 - ppois(0, 4.43)
+
+pbinom(1, 96, 0.01) - pbinom(0, 96, 0.01)
+pbinom(1, 96, 0.01) - dbinom(0, 96, 0.01)
+dbinom(0, 96, 0.01) + dbinom(1, 96, 0.01)
