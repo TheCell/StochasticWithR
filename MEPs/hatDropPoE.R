d2575d58a76b4deda07705ae000a64fe26d6a93e
TheCell <simon_hischier@hotmail.com>
2017-12-04 00:54:35 +0100

    could not sleep, still thinking

--- MEPs/hatDropPoE.R
+++ MEPs/hatDropPoE.R
@@ -8,3 +8,5 @@ ppois(0, 0.96)
 pbinom(1, 96, 0.01) - pbinom(0, 96, 0.01)
 pbinom(1, 96, 0.01) - dbinom(0, 96, 0.01)
 dbinom(0, 96, 0.01) + dbinom(1, 96, 0.01)
+
+barplot(dbinom(80:110, 96, 0.96), 10, horiz = FALSE)
